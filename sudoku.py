137fd4a0e8160985b56268bf3cfd549167e30642
Mateusz Bauer <mateusz.bauer@intel.com>
2022-05-26 21:43:32 +0200

    Add constraint programming sudoku solver

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import numpy as np
+from constraint import *
 
 GRID_SIZE = 9
 BLOCK_SIZE = 3
@@ -89,7 +90,67 @@ class Sudoku:
                     self.grid[row, col] = 0
             
         return False
-                
+
+    def solve_constraints(self):
+        problem = Problem()
+
+        # 9 rows of 9 variables, each variable rangin 1..9
+        for i in range(1, 10):
+            problem.addVariables(range(i*10 + 1, i*10 + 10), range(1, 10))
+
+        # Each row has different values
+        for i in range(1, 10):
+            problem.addConstraint(AllDifferentConstraint(), range(i*10 + 1, i*10 + 10))
+        
+        # Each column has different values
+        for i in range(1, 10):
+            problem.addConstraint(AllDifferentConstraint(), range(i+10, 100+i, 10))
+
+        # Each block has different values
+        problem.addConstraint(
+            AllDifferentConstraint(), [11, 12, 13, 21, 22, 23, 31, 32, 33]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [41, 42, 43, 51, 52, 53, 61, 62, 63]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [71, 72, 73, 81, 82, 83, 91, 92, 93]
+        )
+
+        problem.addConstraint(
+            AllDifferentConstraint(), [14, 15, 16, 24, 25, 26, 34, 35, 36]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [44, 45, 46, 54, 55, 56, 64, 65, 66]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [74, 75, 76, 84, 85, 86, 94, 95, 96]
+        )
+
+        problem.addConstraint(
+            AllDifferentConstraint(), [17, 18, 19, 27, 28, 29, 37, 38, 39]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [47, 48, 49, 57, 58, 59, 67, 68, 69]
+        )
+        problem.addConstraint(
+            AllDifferentConstraint(), [77, 78, 79, 87, 88, 89, 97, 98, 99]
+        )
+
+        for i in range(1, 10):
+            for j in range(1, 10):
+                if self.grid[i-1, j-1] != 0:
+                    problem.addConstraint(
+                        lambda var, val=self.grid[i-1, j-1]: var == val, (i*10 + j,)
+                    )
+        
+        solution = problem.getSolution()
+        for i in range(1, 10):
+            for j in range(1, 10):
+                idx = i*10 + j
+                self.grid[i-1, j-1] = solution[idx]
+
+         
 
 def _is_field_valid(field):
     """
