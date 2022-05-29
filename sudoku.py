e1535a44d08d602131f4a159070a223967bfbd5a
Mateusz Bauer <mateusz.bauer@intel.com>
2022-05-29 19:32:56 +0200

    improve constraints method

--- sudoku.py
+++ sudoku.py
@@ -107,36 +107,12 @@ class Sudoku:
             problem.addConstraint(AllDifferentConstraint(), range(i+10, 100+i, 10))
 
         # Each block has different values
-        problem.addConstraint(
-            AllDifferentConstraint(), [11, 12, 13, 21, 22, 23, 31, 32, 33]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [41, 42, 43, 51, 52, 53, 61, 62, 63]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [71, 72, 73, 81, 82, 83, 91, 92, 93]
-        )
+        for i in [1, 4, 7]:
+            for j in [1, 4, 7]:
+                block = [((i+z)*10 + j+y) for z in range(0,BLOCK_SIZE) for y in range(0, BLOCK_SIZE)]
+                problem.addConstraint(AllDifferentConstraint(), block)
 
-        problem.addConstraint(
-            AllDifferentConstraint(), [14, 15, 16, 24, 25, 26, 34, 35, 36]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [44, 45, 46, 54, 55, 56, 64, 65, 66]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [74, 75, 76, 84, 85, 86, 94, 95, 96]
-        )
-
-        problem.addConstraint(
-            AllDifferentConstraint(), [17, 18, 19, 27, 28, 29, 37, 38, 39]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [47, 48, 49, 57, 58, 59, 67, 68, 69]
-        )
-        problem.addConstraint(
-            AllDifferentConstraint(), [77, 78, 79, 87, 88, 89, 97, 98, 99]
-        )
-
+        # Add init values
         for i in range(1, 10):
             for j in range(1, 10):
                 if self.grid[i-1, j-1] != 0:
@@ -144,6 +120,7 @@ class Sudoku:
                         lambda var, val=self.grid[i-1, j-1]: var == val, (i*10 + j,)
                     )
         
+        # Gather result
         solution = problem.getSolution()
         for i in range(1, 10):
             for j in range(1, 10):
