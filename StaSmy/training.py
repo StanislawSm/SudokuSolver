0e82b48df6e09671634dda08cd4035ac8b6b2102
StanislawSm <105853049+StanislawSm@users.noreply.github.com>
2022-05-30 23:10:36 +0200

    fine tunning

--- StaSmy/training.py
+++ StaSmy/training.py
@@ -8,5 +8,5 @@ model = get_model()
 
 adam = keras.optimizers.Adam(lr=.001)
 model.compile(loss='sparse_categorical_crossentropy', optimizer=adam)
-
+    
 model.fit(x_train, y_train, batch_size=32, epochs=2)
